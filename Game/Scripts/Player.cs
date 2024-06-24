14dc1c031cd371be07ba0ec7ac423dcc4ab7ca3d
Figgity / ZMCLN <zandermclane@gmail.com>
2024-06-24 13:47:31 -0400

    Added rudimentary Y-Sort & testing background

--- Game/Scripts/Player.cs
+++ Game/Scripts/Player.cs
@@ -3,16 +3,16 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
+	public const float Speed = 100.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		//For some reason, you cannot move down-left or down-right with specifically WSAD
-		//I've put inquires into this, but maybe its just my keyboard.
-		//At the very least, I know its with the input not the code. Check, I also mapped with arrow keys.
-		Vector2 input =  Input.GetVector("left", "right", "up", "down");
+		Vector2 input = Input.GetVector("left", "right", "up", "down");
+
+		//This will need to be tweaked for sure
+		input.Y /= 2;
 
         if (input != Vector2.Zero)
 		{
