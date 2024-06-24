1c558bd9ac5e6a68b117fd83132653d6d847f611
Figgity / ZMCLN <zandermclane@gmail.com>
2024-06-24 13:01:28 -0400

    Movement code now correct for beat-em-up
    For some reason WSAD seems to be janky (I cannot move down-left or down-right).  Unsure if this is my keyboard, but arrow keys work fine mapped to the same inputs.
    
    I've put inquiries out on this, but we might just use arrow keys or something.

--- Game/Scripts/Player.cs
+++ Game/Scripts/Player.cs
@@ -4,34 +4,26 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+		//For some reason, you cannot move down-left or down-right with specifically WSAD
+		//I've put inquires into this, but maybe its just my keyboard.
+		//At the very least, I know its with the input not the code. Check, I also mapped with arrow keys.
+		Vector2 input =  Input.GetVector("left", "right", "up", "down");
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+        if (input != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			velocity = input.Normalized() * Speed;			
 		}
 		else
 		{
+			//This could be done better
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+            velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+        }
 
 		Velocity = velocity;
 		MoveAndSlide();
